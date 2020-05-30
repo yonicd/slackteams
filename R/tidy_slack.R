e74d4f0fc2e6b2e490bb56ad89dfb0f79e292928
Jonathan Sidi <yonicd@gmail.com>
2020-05-30 01:54:12 -0400

    replace purrr with data.table

--- R/tidy_slack.R
+++ R/tidy_slack.R
@@ -8,34 +8,40 @@ tidy_slack.conversations_members <- function(res) {
 }
 
 #' @importFrom tibble as_tibble
-#' @importFrom purrr map_df
+#' @importFrom data.table rbindlist
 tidy_slack.users_list <- function(res) {
   members <- res$members
 
   cols <- lapply(members, names)
 
-  member_info <- purrr::map_df(members, function(x) {
+  member_info_list <- lapply(members, function(x) {
     cols <- setdiff(names(x), c("profile", "real_name", "tz"))
     tibble::as_tibble(x[cols])
   })
 
-  member_profile <- purrr::map_df(members, function(x) {
+  member_info <- tibble::as_tibble(data.table::rbindlist(member_info_list,fill=TRUE))
+
+  member_profile_list <- lapply(members, function(x) {
     cols <- setdiff(names(x$profile), c("fields"))
     tibble::as_tibble(x$profile[cols])
   })
+
+  member_profile <- tibble::as_tibble(data.table::rbindlist(member_profile_list,fill=TRUE))
 
   tibble::as_tibble(cbind.data.frame(member_info, member_profile, stringsAsFactors = FALSE))
 }
 
 #' @importFrom tibble as_tibble
-#' @importFrom purrr map_df map_lgl
+#' @importFrom data.table rbindlist
 tidy_slack.default <- function(res) {
   fields <- c("channel", "channels", "groups", "conversations","members")
 
   el <- intersect(names(res), fields)
 
-  purrr::map_df(res[[el]], function(x) {
-    cols <- !purrr::map_lgl(x, inherits, what = c("NULL", "list"))
+  el_list <- lapply(res[[el]], function(x) {
+    cols <- !unlist(lapply(x, inherits, what = c("NULL", "list")))
     tibble::as_tibble(x[cols])
   })
+
+  tibble::as_tibble(data.table::rbindlist(el_list,fill=TRUE))
 }
