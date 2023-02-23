5fd3d9b93740824788948467c81cf391e05871af
Jon Harmon <jonthegeek@gmail.com>
2023-02-23 11:21:02 -0600

    Update GHA.
    Mock except on main/master.

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,11 @@
 .onLoad <- function(lib,pkg) {
-
-if(isFALSE(as.logical(Sys.getenv('SLACK_SKIPLOAD')))){
-  if(nzchar(Sys.getenv('SLACK_API_TOKEN'))&interactive()){
-    info <- get_team_info(token = Sys.getenv('SLACK_API_TOKEN'))
-    add_team(info$team$name,Sys.getenv('SLACK_API_TOKEN'))
-    activate_team(info$team$name)
+  if (isFALSE(as.logical(Sys.getenv('SLACK_SKIPLOAD')))) {
+    if (nzchar(Sys.getenv('SLACK_API_TOKEN')) & interactive()) {
+      info <- get_team_info(token = Sys.getenv('SLACK_API_TOKEN'))
+      add_team(info$team$name, Sys.getenv('SLACK_API_TOKEN'))
+      activate_team(info$team$name)
+    }
   }
-}
 }
 
 .onAttach <- function(lib,pkg) {
