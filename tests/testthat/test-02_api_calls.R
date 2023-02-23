e1316a3049c9a64c122725215375f677ca474c45
Jon Harmon <jonthegeek@gmail.com>
2023-02-23 11:57:05 -0600

    Remove snapshot tests.

--- tests/testthat/test-02_api_calls.R
+++ tests/testthat/test-02_api_calls.R
@@ -46,17 +46,41 @@ if (slack_api_test_mode == "true" || slack_api_test_mode == "capture") {
 # Tests. -----------------------------------------------------------------------
 
 test_that("User list loads", {
-  expect_snapshot(
+  expect_error(
     with_mock_api({
-      get_users_list()
-    })
+      res_user_list <- get_users_list()
+    }),
+    NA
+  )
+
+  expect_s3_class(
+    res_user_list,
+    "data.frame"
+  )
+
+  expect_identical(
+    colnames(res_user_list),
+    c(
+      "id", "team_id", "name", "deleted", "color", "real_name", "tz",
+      "tz_label", "tz_offset", "is_admin", "is_owner", "is_primary_owner",
+      "is_restricted", "is_ultra_restricted", "is_bot", "is_app_user",
+      "updated", "is_email_confirmed", "who_can_share_contact_card",
+      "has_2fa"
+    )
+  )
+
+  expect_identical(
+    nrow(res_user_list),
+    3L
+  )
+
+  expect_identical(
+    res_user_list$id,
+    c("USLACKBOT", "UNTFB8LTH", "U0111B8S0LX")
   )
 })
 
-test_that("Channel info loads", {
-  # We can't just use snapshots here, because different valid users in capture
-  # mode can get different IM results.
-
+test_that("Conversation list loads", {
   expect_error(
     with_mock_api({
       res_convo_list <- get_conversations_list()
@@ -64,28 +88,96 @@ test_that("Channel info loads", {
     NA
   )
 
-  # Get rid of the bits that can change.
-  res_convo_list <- res_convo_list[!res_convo_list$is_im, ]
-  res_convo_list$updated <- NULL
+  expect_s3_class(
+    res_convo_list,
+    "data.frame"
+  )
 
-  expect_snapshot(res_convo_list)
+  expect_identical(
+    colnames(res_convo_list),
+    c(
+      "id", "name", "is_channel", "is_group", "is_im", "is_mpim",
+      "is_private", "created", "is_archived", "is_general", "unlinked",
+      "name_normalized", "is_shared", "is_org_shared", "is_pending_ext_shared",
+      "context_team_id", "updated", "creator", "is_ext_shared", "is_member",
+      "num_members", "user", "is_user_deleted", "priority"
+    )
+  )
 
+  expect_identical(
+    nrow(res_convo_list[!res_convo_list$is_im, ]),
+    3L
+  )
+
+  expect_identical(
+    res_convo_list[!res_convo_list$is_im, ]$id,
+    c("CNRKL1H6C", "CNRKL1JLQ", "CNTFB9215")
+  )
+})
+
+test_that("Convo info loads", {
   expect_error(
     with_mock_api({
-      convo_info <- get_conversations_info(slack_test_channel)
+      res_convo_info <- get_conversations_info(slack_test_channel)
     }),
     NA
   )
 
-  # Get rid of the bits that can change.
-  convo_info$channel$last_read <- NULL
-  convo_info$channel$updated <- NULL
+  expect_s3_class(
+    res_convo_info,
+    c("conversations.info", "list")
+  )
 
-  expect_snapshot(convo_info)
+  expect_identical(
+    names(res_convo_info),
+    c("ok", "channel")
+  )
 
-  expect_snapshot(
+  expect_true(res_convo_info$ok)
+
+  expect_identical(
+    names(res_convo_info$channel),
+    c(
+      "id", "name", "is_channel", "is_group", "is_im", "is_mpim",
+      "is_private", "created", "is_archived", "is_general", "unlinked",
+      "name_normalized", "is_shared", "is_org_shared", "is_pending_ext_shared",
+      "pending_shared", "context_team_id", "updated", "parent_conversation",
+      "creator", "is_ext_shared", "shared_team_ids", "pending_connected_team_ids",
+      "is_member", "last_read", "topic", "purpose", "previous_names"
+    )
+  )
+
+  expect_identical(
+    res_convo_info$channel$id,
+    "CNTFB9215"
+  )
+})
+
+test_that("Convo members load", {
+  expect_error(
     with_mock_api({
-      get_conversations_members(slack_test_channel)
-    })
+      res_convo_members <- get_conversations_members(slack_test_channel)
+    }),
+    NA
+  )
+
+  expect_s3_class(
+    res_convo_members,
+    "data.frame"
+  )
+
+  expect_identical(
+    colnames(res_convo_members),
+    "value"
+  )
+
+  expect_identical(
+    nrow(res_convo_members),
+    2L
+  )
+
+  expect_identical(
+    res_convo_members$value,
+    c("UNTFB8LTH", "U0111B8S0LX")
   )
 })
