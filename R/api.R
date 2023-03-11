d866371f77a7f0cbb202d946f4ee9c03a312d904
Jonathan Sidi <yonis@metrumrg.com>
2023-03-11 09:41:08 -0500

    remove import of parse_call from slackcalls

--- R/api.R
+++ R/api.R
@@ -3,7 +3,7 @@
 #' @importFrom slackcalls post_slack
 get_users_list <- function(..., token = Sys.getenv("SLACK_API_TOKEN"), max_results = Inf, max_calls = Inf) {
 
-  res <- slackcalls::post_slack(slackcalls::parse_call(), max_results = max_results, max_calls = max_calls, token = token, ...)
+  res <- slackcalls::post_slack('users.list', max_results = max_results, max_calls = max_calls, token = token, ...)
 
   tidy_slack(res)
 }
@@ -25,7 +25,7 @@ get_users_list <- function(..., token = Sys.getenv("SLACK_API_TOKEN"), max_resul
 get_conversations_list <- function(type = c("public_channel", "private_channel", "mpim", "im"), ..., token = Sys.getenv("SLACK_API_TOKEN"), max_results = Inf, max_calls = Inf) {
   types <- paste0(match.arg(type, several.ok = TRUE), collapse = ",")
 
-  res <- slackcalls::post_slack(slackcalls::parse_call(), max_results = max_results, max_calls = max_calls, token = token, types = types, ...)
+  res <- slackcalls::post_slack('conversations.list', max_results = max_results, max_calls = max_calls, token = token, types = types, ...)
 
   tidy_slack(res)
 }
@@ -33,14 +33,14 @@ get_conversations_list <- function(type = c("public_channel", "private_channel",
 #' @importFrom slackcalls post_slack
 get_conversations_info <- function(channel, ..., token = Sys.getenv("SLACK_API_TOKEN"), max_results = Inf, max_calls = Inf) {
 
-  res <- slackcalls::post_slack(slackcalls::parse_call(), max_results = max_results, max_calls = max_calls, channel = channel, token = token, ...)
+  res <- slackcalls::post_slack('conversations.info', max_results = max_results, max_calls = max_calls, channel = channel, token = token, ...)
 
   return(res)
 }
 
 #' @importFrom slackcalls post_slack
 get_conversations_members <- function(channel, ..., token = Sys.getenv("SLACK_API_TOKEN"), max_results = Inf, max_calls = Inf) {
-  res <- slackcalls::post_slack(slackcalls::parse_call(), max_results = max_results, max_calls = max_calls, channel = channel, token = token, ...)
+  res <- slackcalls::post_slack('conversations.members', max_results = max_results, max_calls = max_calls, channel = channel, token = token, ...)
 
   tidy_slack(res)
 }
@@ -49,7 +49,7 @@ get_conversations_members <- function(channel, ..., token = Sys.getenv("SLACK_AP
 
 #' @importFrom slackcalls post_slack
 get_team_info <- function(team = NULL, ..., token = Sys.getenv("SLACK_API_TOKEN")) {
-  res <- slackcalls::post_slack(slackcalls::parse_call(), team = team, token = token, ...)
+  res <- slackcalls::post_slack('team.info', team = team, token = token, ...)
 
   res
 }
